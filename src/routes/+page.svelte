efefbf1bf7e8e9ad5a0dd67d250963c7da3b50a2
Loïc Monard <loicmonard@gmail.com>
2025-03-25 11:38:52 +0100

    feat: add a first grid system - Add an aspect-ratio's tailwind package - Add a first grid layout
    Signed-off-by: Loïc Monard <loicmonard@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1 class="text-red-500">Welcome to red SvelteKit portfolio</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+ <div class="max-w-screen-xl mx-auto p-4">
+	<div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
+		<!-- Première ligne -->
+		<div class="col-span-2 row-span-2">
+			<div class="w-full aspect-w-1 aspect-h-1 bg-blue-500"></div>
+		</div>
+		<div class="col-span-1 row-span-1">
+			<div class="w-full aspect-w-1 aspect-h-1 bg-green-500"></div>
+		</div>
+		<div class="col-span-2 row-span-1">
+			<div class="w-full aspect-w-2 aspect-h-1 bg-red-500"></div>
+		</div>
+
+		<!-- Deuxième ligne -->
+		<div class="col-span-1 row-span-1">
+			<div class="w-full aspect-w-1 aspect-h-1 bg-yellow-500"></div>
+		</div>
+		<div class="col-span-2 row-span-2">
+			<div class="w-full aspect-w-1 aspect-h-1 bg-purple-500"></div>
+		</div>
+
+		<!-- Troisième ligne -->
+		<div class="col-span-2 row-span-1">
+			<div class="w-full aspect-w-2 aspect-h-1 bg-pink-500"></div>
+		</div>
+		<div class="col-span-1 row-span-1">
+			<div class="w-full aspect-w-1 aspect-h-1 bg-indigo-500"></div>
+		</div>
+	</div>
+</div>
