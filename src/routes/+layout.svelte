c0af8eb9c1f48026acbd2acf09fb86b396a69fdd
Loïc Monard <loicmonard@gmail.com>
2025-04-09 17:17:55 +0200

    style: update CSS imports and add Google Fonts link in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
+
+<svelte:head>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Gabarito:wght@400;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
 
 <slot />
