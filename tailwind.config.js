f4623071eb2c9621355f4de4cbebe5e7711036f2
Loïc Monard <loicmonard@gmail.com>
2025-04-09 22:30:39 +0200

    feat: extend TailwindCSS theme with custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				pastel: {
+					yellow: '#FFF582',
+					white: '#F9F9F6'
+				},
+				text: {
+					light: '#6B6B6B',
+					dark: '#333333'
+				},
+				accent: {
+					beige: '#F3EED9',
+					sage: '#DCE5D1'
+				}
+			}
+		}
 	},
 	plugins: []
 };
-
