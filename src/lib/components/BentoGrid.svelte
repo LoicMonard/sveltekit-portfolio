64b1ed2f33df955cd4da53248ead2a0f79b5d2b7
Loïc Monard <loicmonard@gmail.com>
2025-04-21 11:16:57 +0200

    refactor: update BentoGrid layout for improved responsiveness and design

--- src/lib/components/BentoGrid.svelte
+++ src/lib/components/BentoGrid.svelte
@@ -1,53 +1,55 @@
 <script lang="ts">
 </script>
 
-<div class="flex flex-wrap justify-start gap-4">
-	<div
-		class="flex max-w-[50%] flex-1 items-center justify-center rounded-lg border-2 border-accent-beige bg-pastel-white px-6 py-4 text-slate-600"
-	></div>
-	<div
-		class="flex aspect-square w-[150px] items-center justify-center rounded-lg text-slate-600 sm:w-[180px] md:w-[200px]"
-	>
-		<div class="grid h-full w-full grid-cols-2 gap-2">
-			<div
-				class="flex cursor-pointer items-center justify-center rounded-lg border-2 border-[#f7dcae] bg-[#FAE4BD] transition-transform duration-300 hover:scale-105"
-			>
-				<img
-					src="/svelte_logo_200x200.png"
-					alt="svelte logo"
-					class="drop-shadow-[-4px_2px_2px_rgba(171,69,10,0.3)] filter"
-				/>
-			</div>
-			<div
-				class="flex cursor-pointer items-center justify-center rounded-lg border-2 border-[#d5e8bc] bg-[#DCECCC] transition-transform duration-300 hover:scale-105"
-			>
-				<img
-					src="/vuejs_logo_200x200.png"
-					alt="vuejs logo"
-					class="drop-shadow-[-4px_2px_2px_rgba(62,99,79,.3)] filter"
-				/>
-			</div>
-			<div
-				class="flex cursor-pointer items-center justify-center rounded-lg border-2 border-[#f6ed76] bg-pastel-yellow transition-transform duration-300 hover:scale-105"
-			>
-				<img
-					src="/js_logo_no_bg_200x200.png"
-					alt="vuejs logo"
-					class="drop-shadow-[-4px_2px_2px_rgba(0,0,0,0.3)] filter"
-				/>
-			</div>
-			<div
-				class="flex cursor-pointer items-center justify-center rounded-lg border-2 border-[#c7dedd] bg-[#D0E3E1] transition-transform duration-300 hover:scale-105"
-			>
-				<img
-					src="/css3_logo_200x200.png"
-					alt="vuejs logo"
-					class="drop-shadow-[-4px_2px_2px_rgba(0,0,0,0.2)] filter"
-				/>
+<div class="grid auto-rows-[250px] grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-4">
+	<div class="rounded-lg border bg-white lg:col-span-1 lg:row-span-1"></div>
+
+	<div class="rounded-lg border bg-white lg:col-span-2 lg:row-span-1"></div>
+
+	<div class="rounded-lg border bg-white lg:col-span-1 lg:row-span-2"></div>
+
+	<div class="rounded-lg border bg-white lg:col-span-2 lg:row-span-1"></div>
+
+	<div class="lg:col-span-1 lg:row-span-1">
+		<div class="flex h-full items-center justify-center rounded-lg text-slate-600">
+			<div class="grid h-full w-full grid-cols-2 grid-rows-2 gap-2">
+				<div
+					class="flex items-center justify-center rounded-lg border-2 border-[#f7dcae] bg-[#FAE4BD] transition-transform duration-300 hover:scale-105"
+				>
+					<img
+						src="/svelte_logo_200x200.png"
+						alt="svelte logo"
+						class="h-3/4 max-h-[80px] object-contain drop-shadow-[-4px_2px_2px_rgba(171,69,10,0.3)]"
+					/>
+				</div>
+				<div
+					class="flex items-center justify-center rounded-lg border-2 border-[#d5e8bc] bg-[#DCECCC] transition-transform duration-300 hover:scale-105"
+				>
+					<img
+						src="/vuejs_logo_200x200.png"
+						alt="vuejs logo"
+						class="h-3/4 max-h-[80px] object-contain drop-shadow-[-4px_2px_2px_rgba(62,99,79,.3)]"
+					/>
+				</div>
+				<div
+					class="flex items-center justify-center rounded-lg border-2 border-[#f6ed76] bg-pastel-yellow transition-transform duration-300 hover:scale-105"
+				>
+					<img
+						src="/js_logo_no_bg_200x200.png"
+						alt="js logo"
+						class="h-3/4 max-h-[80px] object-contain drop-shadow-[-4px_2px_2px_rgba(0,0,0,0.3)]"
+					/>
+				</div>
+				<div
+					class="flex items-center justify-center rounded-lg border-2 border-[#c7dedd] bg-[#D0E3E1] transition-transform duration-300 hover:scale-105"
+				>
+					<img
+						src="/css3_logo_200x200.png"
+						alt="css logo"
+						class="h-3/4 max-h-[80px] object-contain drop-shadow-[-4px_2px_2px_rgba(0,0,0,0.2)]"
+					/>
+				</div>
 			</div>
 		</div>
 	</div>
-	<div
-		class=" flex flex-1 items-center justify-center rounded-lg border-2 border-accent-beige bg-pastel-white px-6 py-4 text-slate-600"
-	></div>
 </div>
