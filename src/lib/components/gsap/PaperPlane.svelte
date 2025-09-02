a5c0cd61f668a83f895a612d34099931ab26a374
Loïc Monard <monard@intechinfo.fr>
2025-09-02 15:42:34 +0200

    fix: rename gsap components

---
+++ src/lib/components/gsap/PaperPlane.svelte
@@ -0,0 +1,24 @@
+<svg id="planeSvg" viewBox="-2 -2 81 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M12 30L10.4 25L78 30H12Z"
+		fill="#4F4B45"
+		fill-opacity="0.5"
+		stroke="#54514C"
+		stroke-width="2"
+	/>
+	<path d="M10 30H78H17H10Z" stroke="#54514C" stroke-width="2" />
+	<path
+		d="M78 30L10 23L9.09735 3.59292C9.04433 2.45311 9.95414 1.5 11.0952 1.5H25.9835C26.3223 1.5 26.6555 1.58605 26.9519 1.75009L78 30Z"
+		fill="white"
+		stroke="#54514C"
+		stroke-width="2"
+	/>
+	<path
+		d="M1.26932 46.5883L10 30H78L22.5416 52.7776C22.1879 52.9228 21.8001 52.9637 21.424 52.8953L2.68139 49.4875C1.35244 49.2459 0.64022 47.7836 1.26932 46.5883Z"
+		fill="white"
+		stroke="#54514C"
+		stroke-width="2"
+	/>
+	<path d="M26.5 1.5L32.5 25.5" stroke="#54514C" stroke-width="0.5" />
+	<path d="M32 30L22 53" stroke="#54514C" stroke-width="0.5" />
+</svg>
