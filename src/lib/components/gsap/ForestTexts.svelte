c1b49eef2927d9ada821ed0ce30a1b94811fa825
Loïc Monard <monard@intechinfo.fr>
2025-10-10 16:27:16 +0200

    refactor: improve formatting and structure of ForestTexts component

--- src/lib/components/gsap/ForestTexts.svelte
+++ src/lib/components/gsap/ForestTexts.svelte
@@ -1,6 +1,26 @@
-<div id="forestKeywords" class="w-full h-full text-4xl md:text-6xl lg:text-8xl">
-  <p id="creativeText" class="invisible top-[30%] left-[10%] md:left-[20%] absolute keyword font-bold text-slate-700">creative</p>
-  <p id="creativeText" class="invisible top-[35%] right-[10%] md:right-[20%] absolute keyword font-bold text-slate-700">developer</p>
-  <p id="creativeText" class="invisible top-[50%] left-[10%] md:left-[20%] absolute keyword font-bold text-slate-700">unique</p>
-  <p id="creativeText" class="invisible top-[55%] right-[10%] md:right-[20%] absolute keyword font-bold text-slate-700">experience</p>
+<div id="forestKeywords" class="h-full w-full text-4xl md:text-6xl lg:text-8xl">
+	<p
+		id="creativeText"
+		class="keyword invisible absolute left-[10%] top-[30%] font-bold text-slate-700 md:left-[20%]"
+	>
+		creative
+	</p>
+	<p
+		id="creativeText"
+		class="keyword invisible absolute right-[10%] top-[35%] font-bold text-slate-700 md:right-[20%]"
+	>
+		developer
+	</p>
+	<p
+		id="creativeText"
+		class="keyword invisible absolute left-[10%] top-[50%] font-bold text-slate-700 md:left-[20%]"
+	>
+		unique
+	</p>
+	<p
+		id="creativeText"
+		class="keyword invisible absolute right-[10%] top-[55%] font-bold text-slate-700 md:right-[20%]"
+	>
+		experience
+	</p>
 </div>
