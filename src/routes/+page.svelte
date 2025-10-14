d02f82971d4f4d864f934fc58a6e769ed1c8a494
Loïc Monard <monard@intechinfo.fr>
2025-10-14 18:52:07 +0200

    feat: reorganize imports and add projects component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,30 @@
 <script lang="ts">
-	import { onMount, setContext } from 'svelte';
+	import { dev } from '$app/environment';
+	import { buildCityFeature } from '$lib/anim/features/city';
+	import { buildForestFeature } from '$lib/anim/features/forest';
+	import { buildIntroGrid } from '$lib/anim/features/introGrid';
+	import { buildPlaneFeature } from '$lib/anim/features/plane';
+	import { buildSkyFeature } from '$lib/anim/features/sky';
+	import { buildWelcomeText } from '$lib/anim/features/welcomeText';
 	import { createMaster } from '$lib/anim/master';
 	import { RANGES } from '$lib/anim/ranges';
-	import { buildIntroGrid } from '$lib/anim/features/introGrid';
+	import ForestTexts from '$lib/components/gsap/ForestTexts.svelte';
 	import Grid2 from '$lib/components/gsap/Grid2.svelte';
-	import { buildPlaneFeature } from '$lib/anim/features/plane';
-	import PaperPlaneMotionPath from '$lib/components/gsap/PaperPlaneMotionPath.svelte';
-	import PaperPlane2 from '$lib/components/gsap/PaperPlane2.svelte';
 	import PaperPlane from '$lib/components/gsap/PaperPlane.svelte';
-	import { buildWelcomeText } from '$lib/anim/features/welcomeText';
+	import PaperPlane2 from '$lib/components/gsap/PaperPlane2.svelte';
+	import PaperPlaneMotionPath from '$lib/components/gsap/PaperPlaneMotionPath.svelte';
+	import SaintMaloCenter from '$lib/components/gsap/SaintMaloCenter.svelte';
 	import SaintMaloLeft from '$lib/components/gsap/SaintMaloLeft.svelte';
 	import SaintMaloRight from '$lib/components/gsap/SaintMaloRight.svelte';
-	import SaintMaloCenter from '$lib/components/gsap/SaintMaloCenter.svelte';
-	import { buildCityFeature } from '$lib/anim/features/city';
-	import ThreeWind from '$lib/components/gsap/ThreeWind.svelte';
-	import { buildForestFeature } from '$lib/anim/features/forest';
-	import { buildSkyFeature } from '$lib/anim/features/sky';
 	import Sky from '$lib/components/gsap/Sky.svelte';
+	import ThreeWind from '$lib/components/gsap/ThreeWind.svelte';
+	import Projects from '$lib/components/Projects.svelte';
+	import { onMount, setContext } from 'svelte';
 	import { writable, type Writable } from 'svelte/store';
-	import { dev } from '$app/environment';
-	import ForestTexts from '$lib/components/gsap/ForestTexts.svelte';
 
 	let scrollTop: number = 0;
 	let totalRangeHeight = 0;
-	const isExpanded = writable<boolean>(true);
+	const isExpanded = writable<boolean>(false);
 	setContext<Writable<boolean>>('isExpanded', isExpanded);
 
 	const buildFeatures = (ctx: any) => {
@@ -40,6 +41,13 @@
 		buildCityFeature(ctx, RANGES.city, {});
 		buildForestFeature(ctx, RANGES.forest);
 		buildSkyFeature(ctx, RANGES.sky);
+		// const waves = buildWaves(ctx, {
+		// 	containerSel: '#seaContainer',
+		// 	speeds: [3.5, 4, 4.5], // vitesses légèrement différentes pour l'effet parallax
+		// 	verticalMove: 3, // monte de 3px
+		// 	horizontalMove: 80, // avance de 80px
+		// 	debug: false
+		// });
 	};
 
 	const toggleHeight = () => {
@@ -61,14 +69,14 @@
 </script>
 
 <!-- Helpers -->
-<div class="pointer-events-none fixed top-0 z-[100] min-h-[100dvh] w-full">
+<!-- <div class="pointer-events-none fixed top-0 z-[100] min-h-[100dvh] w-full">
 	<div id="bluebox" class="absolute left-[20px] flex flex-row text-lg italic">
 		<div class="min-w-[5ch]">
 			{scrollTop}
 		</div>
 		⚠️ Website under construction ⚠️
 	</div>
-</div>
+</div> -->
 
 {#if dev}
 	<button
@@ -149,6 +157,29 @@
 						<div id="saintMaloRight" class="aspect-[1814/193] basis-[66.10%]">
 							<SaintMaloRight />
 						</div>
+						<!-- <div id="seaContainer" class="flex items-end justify-end fixed bottom-[-40%] w-full h-auto translate-x-[-160px]">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 -10 2904 51"
+							>
+								<path
+									fill="#F5F5F5"
+									stroke="#868686"
+									d="M70 7C50-4.547 36.667 2 30 7v20h2874V7c-7.81 5-23.69 11.547-47.12 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.875 0-23.437-11.547-39.063-5-46.875 0-7.812 5-23.438 11.547-46.875 0S865.312 2 857.5 7c-7.812 5-23.438 11.547-46.875 0s-39.063-5-46.875 0c-7.812 5-23.438 11.547-46.875 0S677.812 2 670 7c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0Z"
+								/>
+								<path
+									stroke="#868686"
+									fill="#fff"
+									d="M55 9C35-2.547 21.667 4 15 9v20h2874V9c-7.81 5-23.69 11.547-47.12 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.875 0-23.437-11.547-39.063-5-46.875 0-7.812 5-23.438 11.547-46.875 0S850.312 4 842.5 9c-7.812 5-23.438 11.547-46.875 0s-39.063-5-46.875 0c-7.812 5-23.438 11.547-46.875 0S662.812 4 655 9c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0Z"
+								/>
+								<path
+									stroke="#868686"
+									fill="#fff"
+									d="M40 11C20-.547 6.667 6 0 11v20h2874V11c-7.81 5-23.69 11.547-47.12 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-6.67 5-20 11.547-40 0s-33.33-5-40 0c-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.44 11.547-46.88 0-23.43-11.547-39.06-5-46.87 0-7.81 5-23.44 11.547-46.87 0-23.44-11.547-39.07-5-46.88 0-7.81 5-23.438 11.547-46.875 0s-39.063-5-46.875 0c-7.812 5-23.438 11.547-46.875 0s-39.063-5-46.875 0c-7.812 5-23.438 11.547-46.875 0s-39.063-5-46.875 0c-7.812 5-23.438 11.547-46.875 0S647.812 6 640 11c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0s-33.333-5-40 0c-6.667 5-20 11.547-40 0Z"
+								/>
+							</svg>
+						</div> -->
 					</div>
 
 					<div class="pointer-events-none absolute left-0 top-0 h-full w-full">
@@ -170,6 +201,6 @@
 	</div>
 
 	{#if dev}
-		<div class="container z-50 h-screen bg-slate-50"></div>
+		<Projects />
 	{/if}
 </div>
